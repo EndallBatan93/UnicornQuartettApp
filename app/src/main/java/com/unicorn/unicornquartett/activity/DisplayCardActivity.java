ba29c30b4cc8eeecfa97774d06412e59bc31ac22
Manuel Hinke <manuel.hinke@uni-ulm.de>
2017-12-16 15:23:27 +0100

    activity for deck display

---
+++ app/src/main/java/com/unicorn/unicornquartett/activity/DisplayCardActivity.java
@@ -0,0 +1,15 @@
+package com.unicorn.unicornquartett.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.unicorn.unicornquartett.R;
+
+public class DisplayCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_card);
+    }
+}
